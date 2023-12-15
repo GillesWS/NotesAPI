f479ae945d01453233fee56e1384ce8975cfdc27
GillesWS <gilles.waegemans@student.thomasmore.be>
2023-12-15 17:53:14 +0100

    Docker compose in browser
    Show input from database in browser url

---
+++ RecipeAPI/Models/Seeder.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture;
+using RecipeAPI.Models;
+
+namespace NoteAPI.Models
+{
+    public static class Seeder
+    {
+        public static void Seed(this NoteContext noteContext)
+        {
+            if (!noteContext.note.Any())
+            {
+                Fixture fixture = new Fixture();
+                fixture.Customize<Note>(note => note.Without(p => p.Id));
+                //--- The next two lines add 100 rows to your database
+                List<Note> notes = fixture.CreateMany<Note>(10).ToList();
+                noteContext.AddRange(notes);
+                noteContext.SaveChanges();
+            }
+        }
+    }
+}
--- RecipeAPI/Program.cs
+++ RecipeAPI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 String connectionstring = builder.Configuration.GetConnectionString("DBConn");
 
-builder.Services.AddDbContext<NoteContext>(opt => opt.UseMySql(connectionstring, ServerVersion.AutoDetect(connectionstring)));
+builder.Services.AddDbContext<NoteContext>(opt => opt.UseSqlServer(connectionstring));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,10 +16,20 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}*/
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var noteContext = scope.ServiceProvider.GetRequiredService<NoteContext>();
+        noteContext.Database.EnsureCreated();
+        noteContext.Seed();
+    }
 }
 
 app.UseAuthorization();
